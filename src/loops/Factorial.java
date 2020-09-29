b294087d6da837a62cc09f905de27723fc559479
George <50669257+HELL0ANTHONY@users.noreply.github.com>
2020-09-29 17:17:48 -0300

    Recursive Factorial

---
+++ src/loops/Factorial.java
@@ -0,0 +1,19 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+
+        System.out.print("Introduce a number: ");
+        var number = scan.nextInt();
+
+        System.out.println("Factorial = " + factorial(number));
+    }
+
+    public static int factorial(int number) {
+        if (number == 0) return 1;
+        return number * factorial(number - 1);
+    }
+}
